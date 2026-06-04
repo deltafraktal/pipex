1d11aab1d7d5374167b6a7893ca219ddfe0bb7df
Dounia Geara <dougea71@gmail.com>
2026-06-04 04:43:35 +0200

    wtf

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dgeara <dgeara@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 21:43:54 by dgeara            #+#    #+#             */
-/*   Updated: 2026/06/04 04:30:43 by dgeara           ###   ########.fr       */
+/*   Updated: 2026/06/04 04:43:20 by dgeara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,13 @@ int	main(int ac, char **av, char **envp)
     (void)ac;
     (void)*envp;
     int     pipe_fds[2];
-    int     more_fds[2];
     pid_t   pid;
 
     if (ac != 5)
 		return (ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2\n", 1), 1);
     if (pipe(pipe_fds) == -1)
         return (perror("pipe"), 1);
-    ft_printf("pipe créé ! pipe_fd[0]=%d  pipe_fd[1]=%d\n", pipe_fds[0], pipe_fds[1]);
-
-    if (pipe(more_fds) == -1)
-        return (perror("pipe"), 1);
-    ft_printf("pipe créé ! pipe_fd[0]=%d  pipe_fd[1]=%d\n", more_fds[0], more_fds[1]);
-    
+    //ft_printf("pipe créé ! pipe_fd[0]=%d  pipe_fd[1]=%d\n", pipe_fds[0], pipe_fds[1]);
     pid = fork();
     if (pid < 0)
         return (perror("fork"), 1);
@@ -53,21 +47,17 @@ int	main(int ac, char **av, char **envp)
         close(pipe_fds[0]);                    // on n'a pas besoin de lire
         dup2(pipe_fds[1], STDOUT_FILENO);      // stdout → pipe
         close(pipe_fds[1]);                    // fd original plus nécessaire
-       /*  write(STDOUT_FILENO, "bonjaur\n", 8); // va dans le pipe ! */
         char *args[] = {"/bin/ls", NULL};
         execve("/bin/ls", args, envp);
         perror("execve");
     }
-   pid = fork();
-    if (pid == -1)
-        return (perror("fork"), 1);
-    if (pid == 0)
+    else
     {
         close(pipe_fds[1]);
         dup2(pipe_fds[0], STDIN_FILENO);   // stdin ← pipe
         close(pipe_fds[0]);
-        char *args[] = {"/usr/bin/wc", "-l", NULL};
-        execve("/usr/bin/wc", args, envp);
+        char *args[] = {"/usr/bin/ls", "a", NULL};
+        execve("/usr/bin/grep", args, envp);
         perror("execve");
         exit(1);
     }
@@ -77,45 +67,3 @@ int	main(int ac, char **av, char **envp)
     waitpid(pid, NULL, 0);
     return (0);
 }
-
-
-
-
-/* int	main(int ac, char **av, char **envp)
-{
-    (void)*av;
-    (void)ac;
-    (void)*envp;
-    int pipe_fd[2];
-
-    if (ac != 5)
-		return (ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2\n", 1), 1);
-    if (pipe(pipe_fd) == -1)
-        return (perror("pipe"), 1);
-    ft_printf("pipe créé ! pipe_fd[0]=%d  pipe_fd[1]=%d\n", pipe_fd[0], pipe_fd[1]);
-    
-    pid_t pid = fork();
-    if (pid < 0)
-        return (perror("fork"), 1);
-    if (pid == 0)
-        ft_printf("child process\n");
-    else
-        ft_printf("this is the parent\n");
-    waitpid(pid, NULL, 0);
-    return (0);
-} */
-
-
-/* int	main(int ac, char **av, char **envp)
-{
-    (void)*av;
-    (void)ac;
-    
-    int i;
-    for (i = 0; envp[i] != NULL; i++)
-        printf("\n%s", envp[i]);
-    getchar();
-    return 0;
-} */
-/* char **envp */
-/* pipex(argv, envp) */
